74481fbd15beb9d1da70d1371517a49fac57cad8
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2022-02-11 16:29:26 -0300

    Salva o estoque em uma variável
    Isso evita que zip seja chamado múltiplas vezes, economizando um
    pouquinho de processamento.

--- exercicio_com_vetores_-_celulares.py
+++ exercicio_com_vetores_-_celulares.py
@@ -7,11 +7,13 @@ Uma determinada loja possui 3 tipos de celulares, com suas quantidades e preços
 Este código demonstra que em qualquer posição que você multiplica os valores, seja os estoques, os valores unitários ou o produto interno, o resultado será o mesmo
 '''
 
-estoque = [32, 129, 137] #lista de estoque com a quantidade de celulares
+quantidade = [32, 129, 137] #lista de estoque com a quantidade de celulares
 precos = [1903.65, 729.75, 5069.4] #preço de cada celular
 
+estoque = list(zip(quantidade, precos))
+
 #1
-valor_estoque_celular = [(a * b) for a, b in zip(estoque, precos)] #valor total de cada celular conforme o estoque
+valor_estoque_celular = [(a * b) for a, b in estoque] #valor total de cada celular conforme o estoque
 valor_total_estoque = sum(valor_estoque_celular) #valor total do estoque de todos os celulares
 
 print(f"""Valor total do estoque de cada celular: {valor_estoque_celular}
@@ -19,7 +21,7 @@ Valor total de todo o estoque: {valor_total_estoque}
 """)
 
 #2 dobrando o valor do estoque
-valor_estoque2_celular_prova = [(2 * a * b) for a, b in zip(estoque, precos)] #valor total de cada celular conforme o estoque DOBRADO
+valor_estoque2_celular_prova = [(2 * a * b) for a, b in estoque] #valor total de cada celular conforme o estoque DOBRADO
 valor_total_estoque2_prova = sum(valor_estoque2_celular_prova) #valor total do estoque DOBRADO de todos os celulares
 
 print(f"""Prova do valor total do estoque DOBRADO de cada celular: {valor_estoque2_celular_prova}
@@ -33,7 +35,7 @@ print(f"""Valor total de todo o estoque DOBRADO duplicando o p.i. do estoque nor
 """)
 
 #2 dobrando o valor dos preços unitários
-valor_estoque3_celular = [(a * (2 * b)) for a, b in zip(estoque, precos)]
+valor_estoque3_celular = [(a * (2 * b)) for a, b in estoque]
 valor_total_estoque3 = sum(valor_estoque3_celular)
 
 print(f"""Valor total do estoque de cada celular com o valor unitário DOBRADO: {valor_estoque3_celular}
